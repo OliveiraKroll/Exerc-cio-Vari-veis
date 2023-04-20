149ecd397e48856d5ea8eaad779dc8823f04fa79
johnL <jljohn535@gmail.com>
2023-04-19 21:45:19 -0300

    subindo exercicios de variavaeis

---
+++ index.js
@@ -0,0 +1,48 @@
+//let a = 10
+//let b = 10
+
+//console.log(b)
+
+//b = 5
+//console.log(a, b)
+//Questão 1: console.log(b) = (10) console.log(a,b) = (10, 5)
+
+
+//let a = 10
+//let b = 20
+//c = a
+//b = c
+//a = b
+
+//console.log(a, b, c)
+//Questão 2: console.log(a,b,c) = (10,10,10)
+
+//let p = prompt("Quantas horas você trabalha por dia?")
+//let t = prompt("Quanto você recebe por dia?")
+//alert(`Voce recebe ${t/p} por hora`)
+//Questão 3: (p) numeroDeHorasTrabalhadosPorDia (t) valorPagoPorHora
+
+
+//EXERCICIOS DE ESCRITA DE CODIGO
+
+//1
+const nome = prompt("Digite seu nome")
+console.log(typeof nome)
+const idade = Number (prompt("Digite sua idade"))
+console.log(typeof idade)
+
+//a idade saiu como um numero por causa da conversão
+
+console.log("Olá", nome,", você tem ",idade," anos.")
+
+//2
+let cafe = prompt("Tomou cafe hoje?") 
+console.log("Tomou tomou cafe ?", cafe)
+let dentes = prompt ("Escovou os dentes hoje?")
+console.log("Escovou os dentes hoje?", dentes)
+let banho = prompt("Tomou banho hoje?")
+console.log("Tomou banho hoje?", banho)
+
+
+//3
+
